c9380564a2f3ca5053f69cda5db805f4f4285294
yassinelamkharbech1-rgb <yassinelamkharbech1@gmail.com>
2026-07-23 04:13:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 مرحباً بك في Maroc Education Bot\n\n"
+        "📢 هذا البوت سيرسل آخر الأخبار الرسمية:\n"
+        "• مذكرات وزارة التربية الوطنية\n"
+        "• إعلانات الدكتوراه\n"
+        "• مباريات التعليم\n"
+        "• البلاغات الرسمية"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
